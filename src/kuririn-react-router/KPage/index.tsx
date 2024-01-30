9c4aa7c3da423176fe4130b57ef839ea2d707c6a
jiqishoubi <lntz2014@gmail.com>
2024-01-30 11:58:24 +0800

    optimize: page display none的时候 通过文档碎片缓存页面

--- src/kuririn-react-router/KPage/index.tsx
+++ src/kuririn-react-router/KPage/index.tsx
@@ -1,10 +1,10 @@
-import React, { CSSProperties, PropsWithChildren } from 'react'
+import React, { CSSProperties, PropsWithChildren, useEffect, useLayoutEffect } from 'react'
 import { IPage } from '../store'
 import { getPageKey } from '../utils'
 
 const defaltPageStyle: CSSProperties = {
+  height: '100%',
   overflow: 'auto',
-  height: '100vh',
 }
 
 const KPage: React.FC<
@@ -17,6 +17,34 @@ const KPage: React.FC<
 
   const key = getPageKey(page)
   const classNameStr = '_kuririn_react_router_page' + (page.isTab ? ' _kuririn_react_router_page_tab' : '')
+
+  // 优化 - 通过文档碎片缓存页面
+  useLayoutEffect(() => {
+    if (!isKBlock) {
+      // if (page.isTab) return // tab页面不需要缓存 // 测试
+      const pageDom = document.getElementById(key)
+      if (pageDom) {
+        const fragment = document.createDocumentFragment()
+        fragment.appendChild(pageDom)
+        if (!(window as any)._kuririn_react_router_page_cache) {
+          ;(window as any)._kuririn_react_router_page_cache = {}
+        }
+        ;(window as any)._kuririn_react_router_page_cache[key] = fragment
+        // pageDom.remove() // 不需要移除，加入文档碎片后，会自动移除
+      }
+    } else {
+      if (!(window as any)._kuririn_react_router_page_cache) {
+        return
+      }
+      const fragment = (window as any)._kuririn_react_router_page_cache[key]
+      if (fragment) {
+        const parentDom = document.querySelector(page.isTab ? '._k_pages_container_tab' : '._k_pages_container_normal')
+        if (parentDom) {
+          parentDom.appendChild(fragment)
+        }
+      }
+    }
+  }, [isKBlock])
 
   return (
     <div
