7ada3ed73c6bec2046071834ce27dab33f6b4568
jiqishoubi <lntz2014@gmail.com>
2024-01-30 15:21:50 +0800

    example 目录结构

--- src/example/App.tsx
+++ src/example/App.tsx
@@ -1,16 +1,16 @@
 import { lazy, useEffect } from 'react'
 import { KRouter, IKRouterProps } from '@/kuririn-react-router'
-import TabBar from '@/components/TabBar'
-import PageUserIndex from '@/pages/user/index'
-import PageDetail from '@/pages/detail'
-import PageSubDetail from '@/pages/subDetail'
+import TabBar from '@/example/components/TabBar'
+import PageUserIndex from '@/example/pages/user/index'
+import PageDetail from '@/example/pages/detail'
+import PageSubDetail from '@/example/pages/subDetail'
 
 export const pagesConfig: IKRouterProps['pages'] = [
-  { path: '/', component: lazy(() => import('@/pages/index/index')), isTab: true }, // support lazy loading
+  { path: '/', component: lazy(() => import('@/example/pages/index/index')), isTab: true }, // support lazy loading
   { path: '/user', component: PageUserIndex, isTab: true },
   { path: '/detail', title: '详情', component: PageDetail },
   { path: '/subDetail', title: '二级详情', component: PageSubDetail },
-  { path: '/userSub', title: 'user的子页面', component: lazy(() => import('@/pages/user/useSubPage')) },
+  { path: '/userSub', title: 'user的子页面', component: lazy(() => import('@/example/pages/user/useSubPage')) },
 ]
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './example/App'
 import './index.less'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
